3c18db003fa2faf1952fe494d0f0cea148969c9f
Peter Sankiewicz <petsan@hotmail.com>
2020-06-15 15:31:28 -0700

    implemented demo code into server.js, ps

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 'use strict'
 
+const PORT = process.env.PORT || 3001;
+
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors= require('cors');
-const path = require('path');
 const superagent = require('superagent');
+
 require('ejs');
 require('dotenv').config();
-const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(bodyParser());
-app.use(cors());
-
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('index');
-});
+app.get('/', (request, response) => {
+  let query = request.body.search[0];
+  let titleOrAuthor = request.body.search[1];
 
-app.listen(PORT, function() {
+  let url = 'https://www.googleapi.com/books/v1/volumes?q=';
+
+  if(titleOrAuthor === 'title'){
+    url+=`+intitle:${query}`;
+  }else if(titleOrAuthor === 'author'){
+    url+=`+inauthor:${query}`;
+  }
+
+  superagent.get(url)
+    .then(result => {
+      let bookArray = result.body.items;
+
+      const finalBookArray = bookArray.map(book => {
+        return new Book(book.volumeInfo);
+      })
+      response.render('show.ejs', {searchResults: finalBookArray})
+    })
+})
+
+function Book(info) {
+  // const placdholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+  this.title = info.title ? info.title : 'no title available';
+}
+
+app.listen(PORT, () => {
   console.log(`heard on ${PORT}`);
 });
